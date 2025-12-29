f3cb6db4ca9bd2dbb205d01563dc37ef1620a91a
Rakesh <rakeshv2330@gmail.com>
2025-12-29 15:18:39 +0530

    2 script in learning

---
+++ 02-con-file.sh
@@ -0,0 +1,6 @@
+#! /bin/bash 
+
+echo " ramesh :: hi how are you suresh "
+echo " suresh :: hi i am good what about you "
+echo " ramesh :: i am good and learning shell scripting, do you want to join "
+echo " suresh :: yes i want to learn can you pls let me know how to start "
