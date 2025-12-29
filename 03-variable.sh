9134f99c5a1abc75aa013093aeb090dba43e5b19
Rakesh <rakeshv2330@gmail.com>
2025-12-29 15:32:27 +0530

    this is the scrpit to learning the variables

---
+++ 03-variable.sh
@@ -0,0 +1,9 @@
+#! /bin/bash 
+PERSON1=RAMESH
+PERSON2=SURESH
+
+
+echo " $PERSON1 :: hi how are you suresh "
+echo " $PERSON2 :: hi i am good what about you "
+echo " $PERSON1 :: i am good and learning shell scripting, do you want to join "
+echo " $PERSON2 :: yes i want to learn can you pls let me know how to start "
