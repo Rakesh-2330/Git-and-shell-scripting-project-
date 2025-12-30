b9401670a6eb5d964cfa19b0ef18d6222d159e29
Rakesh <rakeshv2330@gmail.com>
2025-12-30 13:07:41 +0530

    scrpit to check the spical var

---
+++ 08-spical-var.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo " All the variables passed in the script: $@ "
+echo " number of variable passed in the script: $# "
+echo " scrpit name: $0 "
+echo " to see the present working dir : $PWD "
+echo " home dir of the current user: $HOME "
+echo " PID of the current script is : $PID "
+echo " to see the date : $DATE "
+echo " to see the disk size : $df -h "
+echo " to see the free Ram size : $FREE -m
