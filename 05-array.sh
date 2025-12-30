de8ad92cc08702809546f1f0466c5a6485d47595
Rakesh <rakeshv2330@gmail.com>
2025-12-30 11:58:30 +0530

    datatypes scrpit usring arrays

---
+++ 05-array.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Index start with 0, the size is 4
+FRUITS=("APPLE" "KIWI" "MANGO" "ORANGE" ) #ARRAY
+
+echo "First fruit is: ${FRUITS[0]}" 
+echo "Second fruit is: ${FRUITS[1]}"
+echo "Third fruit is: ${FRUITS[2]}"
+echo "forth fruit is: ${FRUITS[3]}"
+
+echo "fruit is: ${FRUITS[@]}"
