2fc9beaae5a8e36ca06c2e22f5b240a242d55a13
Rakesh <rakeshv2330@gmail.com>
2025-12-30 11:39:41 +0530

    new changes in the code

--- 04-variables_username_password.sh
+++ 04-variables_username_password.sh
@@ -1,16 +1,14 @@
 #! /bin/bash
 
-echo " Pleace enter you username :: "
+echo "Pleace enter you username::"
 
+read -s USERNAME #takes the input from user for the username variable
 
-read -S USERNAME # takes the input from user for the username variable
+echo "username entered is $USERNAME"
 
-echo " username entered is $USERNAME "
+echo " pleace enter your password::  "
 
-echo " pleace enter your password "
-
-read  -S PASSWORD # takes the input from user for the password variable
-
+read  -s PASSWORD 
 echo " password entered is $PASSWORD "
 
 
