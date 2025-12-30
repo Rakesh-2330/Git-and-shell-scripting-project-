63b831d3ace63a22aea8174638075913c91b56a1
Rakesh <rakeshv2330@gmail.com>
2025-12-30 11:18:03 +0530

    script for getting username and password

---
+++ 04-variables_username_password.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+echo " Pleace enter you username :: "
+
+
+read USERNAME
+
+echo "username entered is $USERNAME "
+
+echo " pleace enter your password "
+
+read PASSWORD
+
+echo " password entered is $PASSWORD "
+
+
