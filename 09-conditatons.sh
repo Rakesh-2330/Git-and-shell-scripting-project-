2d32a292b3220450a362933155c8d2053f11a046
Rakesh <rakeshv2330@gmail.com>
2025-12-30 13:25:17 +0530

    script for if condition

---
+++ 09-conditatons.sh
@@ -0,0 +1,12 @@
+#! /bin/bash 
+
+Today=(" WHAT IS TODAY " )
+
+if Today=("mon" "tus" "wed" "thu" "fri" ) {
+    echo " we have a class today "
+}
+else {
+    echo " we have no class today "
+}  
+
+fi
