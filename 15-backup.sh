7e119b850724d9fbf979a217991df851625f500a
Rakesh <rakeshv2330@gmail.com>
2025-12-31 18:28:34 +0530

    update the code

--- 15-backup.sh
+++ 15-backup.sh
@@ -3,18 +3,11 @@
 $SOURCE_DIR=$1
 $DEST_DIR=$2
 
-if [ -d $1 ]
+USAGE(){
+    echo -e " USAGE:: sh 15-backup.sh <source> <dest> "
+}
+
+if [$# -lt 0 ]
 then 
-    echo " $1 exists "
-eles
-    echo " $S1 not exists "
-    exit 1
-fi 
-
-if [ -d $2 ]
-then
-    echo " $1 exists "
-else
-    echo " $1 not exists "
+    USAGE
 fi
-
