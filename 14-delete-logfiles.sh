68bb865c75a74676238a8875b7fc8b352662b44d
Rakesh <rakeshv2330@gmail.com>
2025-12-31 17:37:58 +0530

    code to delete

--- 14-delete-logfiles.sh
+++ 14-delete-logfiles.sh
@@ -9,3 +9,14 @@ else
     echo " $SOURCE_DIR is not exists "
     exit 1
 fi
+
+FILES=$(find $SOURCE_DIR -name " *.log " -mtime +14 )
+echo " files: $FILES "
+
+while IFS= read -r line #IFS, internal field seperatpor, empty it will ignore while space.-for not to ignore spical care /
+
+do
+    echo " deleting line:  $line "
+    rm -rf $line
+
+done <<<
