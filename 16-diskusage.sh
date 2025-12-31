2b26de4889204ff205e127b9e24ee8e1f6dc69f0
Rakesh <rakeshv2330@gmail.com>
2025-12-31 22:50:46 +0530

    this is to check the disk in the server

--- 16-diskusage.sh
+++ 16-diskusage.sh
@@ -1,13 +1,9 @@
 #! /bin/bash
 
 DISK_USAGE=$(df -hT | grep xfs )
-DISK_THRESHOLD=5 # IN OUR PROD PROJECT WE WILL SET THIS FOR 75%
+DISK_THRESHOLD=5 #in our prod we are using 75
 
-
-while IFS= read -r line #IFS, internal field seperatpor, empty it will ignore while space.-for not to ignore spical care /
-
-do
+while IFS= read -r line 
+do 
     echo $line
-    
-
 done <<< $DISK_USAGE
